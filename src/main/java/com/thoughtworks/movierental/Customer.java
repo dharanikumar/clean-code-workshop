8ef41ebe05276adfde9c342940074439feef2836
dharanikumar <dharani939@gmail.com>
2019-01-30 15:49:58 +0530

    refactor step3

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -24,7 +24,7 @@ public class Customer {
     int frequentRenterPoints = 0;
     String result = "Rental Record for " + getName() + "\n";
     for (Rental each : rentals) {
-      double thisAmount = amountFor(each);
+      double thisAmount = each.amount();
 
       // add frequent renter points
       frequentRenterPoints++;
@@ -44,27 +44,6 @@ public class Customer {
     result += "You earned " + String.valueOf(frequentRenterPoints)
         + " frequent renter points";
     return result;
-  }
-
-  private double amountFor(Rental rental) {
-    double thisAmount = 0;
-    //determine amounts for each line
-    switch (rental.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-        thisAmount += 2;
-        if (rental.getDaysRented() > 2)
-          thisAmount += (rental.getDaysRented() - 2) * 1.5;
-        break;
-      case Movie.NEW_RELEASE:
-        thisAmount += rental.getDaysRented() * 3;
-        break;
-      case Movie.CHILDRENS:
-        thisAmount += 1.5;
-        if (rental.getDaysRented() > 3)
-          thisAmount += (rental.getDaysRented() - 3) * 1.5;
-        break;
-    }
-    return thisAmount;
   }
 
 }
--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -18,4 +18,24 @@ public class Rental {
     return movie;
   }
 
+  double amount() {
+    double thisAmount = 0;
+    //determine amounts for each line
+    switch (movie.getPriceCode()) {
+      case Movie.REGULAR:
+        thisAmount += 2;
+        if (daysRented > 2)
+          thisAmount += (daysRented - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        thisAmount += daysRented * 3;
+        break;
+      case Movie.CHILDRENS:
+        thisAmount += 1.5;
+        if (daysRented > 3)
+          thisAmount += (daysRented - 3) * 1.5;
+        break;
+    }
+    return thisAmount;
+  }
 }
